0e68525375d811dc8fdf19ff6a00c71ac91f7ba4
Artem Danko <vpeta4141@gmail.com>
2023-03-08 23:11:21 +0200

    feat: update register module

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,14 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  AbstractControl,
-  FormBuilder,
-  FormControl,
-  FormGroup,
-  Validators,
-} from '@angular/forms';
-import { Store } from '@ngrx/store';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 
 import { registerAction } from '../../store/actions/register.action';
+import { isSubmittingSelector } from '../../store/selectors';
 
 @Component({
   selector: 'mc-register',
@@ -17,16 +13,23 @@ import { registerAction } from '../../store/actions/register.action';
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup;
+  isSubmitting$: Observable<boolean>;
 
   constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
     this.initializeForm();
+    this.initializeValues();
+  }
+
+  initializeValues(): void {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector));
+    console.log(this.isSubmitting$, 'isSubmitting');
   }
 
   initializeForm(): void {
     this.form = this.fb.group({
-      username: ['', Validators.required],
+      username: ['', Validators.required, Validators.email],
       email: ['', Validators.required],
       password: ['', Validators.required],
     });
@@ -35,7 +38,6 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(): void {
-    //@ts-ignore
     this.store.dispatch(registerAction(this.form.value));
   }
 }
